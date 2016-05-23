2195801727a16ced73c63145fe961a885f0cf173
TheButton1 <josua.suren@web.de>
2016-05-23 18:19:45 +0200

    pom Properties

--- debtcheckserver-ejb/src/main/java/DebtCheckOnline/DebtCheckOnlineIntegration.java
+++ debtcheckserver-ejb/src/main/java/DebtCheckOnline/DebtCheckOnlineIntegration.java
@@ -6,6 +6,9 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
+import org.jboss.logging.Logger;
+import org.jboss.ws.api.annotation.WebContext;
+
 import Claim.Claim;
 import DebtCheckSession.DebtCheckSession;
 import account.Account;
@@ -14,30 +17,31 @@ import debt.Debt;
 
 
 @WebService
+@WebContext(contextRoot="/xbank")
 @Stateless
 public class DebtCheckOnlineIntegration {
 	
 	private DebtCheckSession getSession(int sessionId) throws NoSessionException {
-		DebtCheckSession session = dao.findSessionById(sessionId);
-		if (session==null)
+		//DebtCheckSession session = dao.findSessionById(sessionId);
+		//if (session==null)
 			throw new NoSessionException("Bitte zunächst ein Login durchführen.");
-		else
-			return session;
+	//	else
+		//	return session;
 	}
 
 	
 	//Hier fehlt noch Response glaube ich (auch als Rückgabewert)
-	public void addNewDebt (int sessionId, int sourceAccount, int targetAccount, BigDecimal amount){
+	public addNewDebtResponse addNewDebt (int sessionId, int sourceAccount, int targetAccount, BigDecimal amount){
 		//TransferMoneyResponse response = new TransferMoneyResponse();
 		try {
 			DebtCheckSession session = getSession(sessionId);
 			Account source = session.getUser();
-			Account target = this.dao.findAccountById(targetAccount);
-			if (source!=null && target!=null) {
+			//Account target = this.dao.findAccountById(targetAccount);
+			//if (source!=null && target!=null) {
 				new Claim(source, amount);
-				new Debt(target, amount);
+				//new Debt(target, amount);
 				//response.setNewBalance(source.getBalance());
-			}
+			//}
 		}
 		catch (DebtCheckException e) {
 			//response.setReturnCode(e.getErrorCode());
