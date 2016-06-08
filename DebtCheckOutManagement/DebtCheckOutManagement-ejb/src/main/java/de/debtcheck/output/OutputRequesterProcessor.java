da20879396c45d0c56f660b901e0d1526dc398de
TheButton1 <josua.suren@web.de>
2016-06-08 18:29:05 +0200

    OutManagement

---
+++ DebtCheckOutManagement/DebtCheckOutManagement-ejb/src/main/java/de/debtcheck/output/OutputRequesterProcessor.java
@@ -0,0 +1,38 @@
+package de.debtcheck.output;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJBException;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import org.jboss.logging.Logger;
+
+
+@MessageDriven(
+		activationConfig = { 
+				@ActivationConfigProperty(
+				propertyName = "destination", propertyValue = "java:/jms/queue/DebtAddQueue"), 
+				@ActivationConfigProperty(
+				propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+				@ActivationConfigProperty(
+				propertyName = "messageSelector",
+				propertyValue = "DocType LIKE 'Letter'")
+		})
+
+public class OutputRequesterProcessor implements MessageListener {
+
+	private static final Logger logger = Logger.getLogger(OutputRequesterProcessor.class);
+	
+	@Override
+	public void onMessage(Message message) {
+       try {
+    	  TextMessage msg = (TextMessage) message;
+          logger.info("Received message from jms/queue/BankingOutput: " + msg.getText());
+       }
+       catch (JMSException e) {
+            throw new EJBException(e);
+       }
+    }
+}
